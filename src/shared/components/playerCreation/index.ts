e77b1c059317e034c6cbea4abd1e99839f9e0fff
Jeremy Noel <bardic.noel@gmail.com>
2025-07-14 18:02:23 -0400

    refactor(player-creation): write as composable parts rather than monolith component

--- src/shared/components/playerCreation/index.ts
+++ src/shared/components/playerCreation/index.ts
@@ -1,7 +1,26 @@
-export { PlayerCreationPage } from './PlayerCreationPage'
-export { AutocompleteSearch } from './AutocompleteSearch'
+// New composable components (recommended)
+export { 
+  PlayerCreationLayout,
+  PlayerCreationContent,
+  PlayerCreationItemsSection,
+  PlayerCreationDetailSection,
+  PlayerCreationEmptyDetail,
+  PlayerCreationFilters
+} from './layout'
+
+// Existing components
 export { MultiAutocompleteSearch } from './MultiAutocompleteSearch'
 export { SelectedTags } from './SelectedTags'
 export { ItemGrid } from './ItemGrid'
 export { DetailPanel } from './DetailPanel'
+export { AutocompleteSearch } from './AutocompleteSearch'
+export { FilterSidebar } from './FilterSidebar'
+export { SearchBar } from './SearchBar'
+export { GenericItemCard } from './GenericItemCard'
+
+// Legacy monolithic component (deprecated - use composable components instead)
+/** @deprecated Use PlayerCreationLayout, PlayerCreationContent, etc. instead */
+export { PlayerCreationPage } from './PlayerCreationPage'
+
+// Types
 export type * from './types' 
---
+++ src/shared/components/playerCreation/layout/index.ts
@@ -0,0 +1,8 @@
+export { PlayerCreationLayout } from './PlayerCreationLayout'
+export { 
+  PlayerCreationContent, 
+  PlayerCreationItemsSection, 
+  PlayerCreationDetailSection, 
+  PlayerCreationEmptyDetail 
+} from './PlayerCreationContent'
+export { PlayerCreationFilters } from './PlayerCreationFilters' 
