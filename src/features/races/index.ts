c3b6016a56d36205a0e395d0ac5a532d7e6704e3
Jeremy Noel <bardic.noel@gmail.com>
2025-07-16 17:45:45 -0400

    race page mvp complete

--- src/features/races/index.ts
+++ src/features/races/index.ts
@@ -1,9 +1,4 @@
-// Components
-export { RaceCard } from './components/RaceCard'
-export { RaceDetailPanel } from './components/RaceDetailPanel'
-
 // Pages
-export { UnifiedRacesPage } from './pages/UnifiedRacesPage'
 export { AccordionRacesPage } from './pages/AccordionRacesPage'
 export { useRaces } from './hooks/useRaces'
 export * from './components'
