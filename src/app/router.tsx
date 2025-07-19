c52cf94b4cbd0815f47352240afb42304f0f862d
Jeremy Noel <bardic.noel@gmail.com>
2025-07-19 15:54:16 -0400

    skills page

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,15 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
 import { AccordionBirthsignsPage } from '@/features/birthsigns/pages/AccordionBirthsignsPage'
-import { AccordionTraitsPage } from '@/features/traits/pages/AccordionTraitsPage'
-import EquipmentPage from '@/pages/EquipmentPage'
-import CraftingPage from '@/pages/CraftingPage'
-import { AccordionSkillsPage } from '@/features/skills'
-import HomePage from '@/pages/HomePage'
-import NotFoundPage from '@/pages/NotFoundPage'
+import { UnifiedDestinyPage } from '@/features/destiny'
 import { AccordionRacesPage } from '@/features/races/pages/AccordionRacesPage'
 import { AccordionReligionsPage } from '@/features/religions/pages/AccordionReligionsPage'
-import { UnifiedDestinyPage } from '@/features/destiny'
+import { AccordionTraitsPage } from '@/features/traits/pages/AccordionTraitsPage'
 import BuildPage from '@/pages/BuildPage'
+import CraftingPage from '@/pages/CraftingPage'
+import EquipmentPage from '@/pages/EquipmentPage'
+import HomePage from '@/pages/HomePage'
+import NotFoundPage from '@/pages/NotFoundPage'
+import { SkillCardDemo } from '@/shared/components/ui/SkillCardDemo'
+import { Route, Routes } from 'react-router-dom'
 
 export const AppRouter = () => {
   return (
@@ -20,7 +20,7 @@ export const AppRouter = () => {
       <Route path="/destiny" element={<UnifiedDestinyPage />} />
       <Route path="/birth-signs" element={<AccordionBirthsignsPage />} />
       <Route path="/traits" element={<AccordionTraitsPage />} />
-      <Route path="/skills" element={<AccordionSkillsPage />} />
+      <Route path="/skills" element={<SkillCardDemo />} />
       <Route path="/religions" element={<AccordionReligionsPage />} />
       <Route path="/equipment" element={<EquipmentPage />} />
       <Route path="/crafting" element={<CraftingPage />} />
